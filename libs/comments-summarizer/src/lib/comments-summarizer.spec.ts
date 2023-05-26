78096a17a8d3a4f5cc53c146de081afa868f4e2b
Karol Binkowski <starystachu9@gmail.com>
2023-05-26 19:09:45 +0200

    test(comments-summarizer): fix test suite and add todo tests

--- libs/comments-summarizer/src/lib/comments-summarizer.spec.ts
+++ libs/comments-summarizer/src/lib/comments-summarizer.spec.ts
@@ -1,7 +1,45 @@
-import { commentsSummarizer } from './comments-summarizer';
+describe('getCommentsSummarizer', () => {
+  it.todo('should return a function');
 
-describe('commentsSummarizer', () => {
-  it('should work', () => {
-    expect(commentsSummarizer()).toEqual('comments-summarizer');
-  });
+  it.todo('should return an empty map if channelId is null');
+
+  it.todo('should call getChannelId with the correct parameters');
+
+  it.todo('should call getCommentsForChannel with the correct parameters');
+
+  it.todo('should return the comments map');
+});
+
+describe('getChannelId', () => {
+  it.todo('should return the channelId from a valid URL');
+
+  it.todo('should call errorCallback with the error if URL parsing fails');
+
+  it.todo('should call errorCallback if it is provided and an error occurs');
+});
+
+describe('getCommentsForChannel', () => {
+  it.todo('should return an empty map if videoIds is empty');
+
+  it.todo('should call getVideoIdsForChannel with the correct parameters');
+
+  it.todo('should call getCommentsForVideo for each videoId');
+
+  it.todo('should call errorCallback if it is provided and an error occurs');
+});
+
+describe('getVideoIdsForChannel', () => {
+  it.todo('should return an empty array if the API response is empty');
+
+  it.todo('should return an array of videoIds from the API response');
+
+  it.todo('should call errorCallback if it is provided and an error occurs');
+});
+
+describe('getCommentsForVideo', () => {
+  it.todo('should return an empty array if the API response is empty');
+
+  it.todo('should return an array of comments from the API response');
+
+  it.todo('should call errorCallback if it is provided and an error occurs');
 });
